2edd5bd0aed6a5e67768bd0165d59cd9007fce77
Siarhei Khaladkou <khaladkou@gmail.com>
2025-08-19 15:34:44 +0200

    refactor: use shared flashcard code

--- build.mjs
+++ build.mjs
@@ -6,13 +6,3 @@ let main = fs.readFileSync('main.js','utf8');
 main = main.replace("import rawSets from './sets/index.js';", `const rawSets = ${JSON.stringify(rawSets)};`);
 fs.writeFileSync('bundle.js', main);
 
-// build standalone flashcard bundles to avoid module loading issues
-function buildFlashcards(name){
-  let cards = fs.readFileSync(`cards/${name}.js`, 'utf8');
-  cards = cards.replace(/\nexport default CARDS;\s*$/, '');
-  let flash = fs.readFileSync('flashcards.js', 'utf8');
-  flash = flash.replace('export default ', '');
-  const bundle = `${cards}\n${flash}\ninitFlashcards(CARDS);\n`;
-  fs.writeFileSync(`${name}.bundle.js`, bundle);
-}
-['adverbs', 'pronouns'].forEach(buildFlashcards);
